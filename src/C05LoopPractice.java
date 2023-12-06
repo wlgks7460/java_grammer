8ea80226efa73dfd30b5d8c516de62a3177297ce
wlgks7460 <wlgks7460@naver.com>
2023-12-06 17:45:32 +0900

    commit

---
+++ src/C05LoopPractice.java
@@ -0,0 +1,46 @@
+public class C05LoopPractice {
+    public static void main(String[] args) {
+
+////        짝수의 합 계산하기
+////        1~20까지 짝수의 합
+//        int sum = 0;
+//        for(int i=1; i <=20 ; i++){
+//            if(i%2 != 0){
+//                continue;
+//            }
+//            sum = sum + i; // sum += i;
+//        }
+//        System.out.println(sum);
+//
+////        숫자 뒤집기
+//        int num = 1234;
+//        int answer = 0;
+//        while(true){
+//            int temp = num % 10;
+//            answer = answer * 10 + temp;
+//            num /= 10;
+//            if (num == 0){
+//                break;
+//            }
+//        }
+//        System.out.println(answer);
+//
+//            최대공약수 구하기
+//            int a = 24;
+//            int b = 36;
+//            int num = a > b ? b : a;
+//            int maxNum = 0;
+//            for(int i=1; i<=num; i++){
+//                if(a%i ==0 && b%i==0){
+//                    maxNum=i;
+//                }
+//            }
+//            System.out.println(maxNum);
+//            int minNum = (a * b)/maxNum;
+//            System.out.println(minNum);
+
+
+
+
+    }
+}
--- src/C05LoopStatements.java
+++ src/C05LoopStatements.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C05LoopStatements {
@@ -20,16 +21,109 @@ public class C05LoopStatements {
 //            a++;
 //        }
 
-        while(true){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("구구단 단수를 입력해주세요");
-            int input = sc.nextInt();
-            int a = 1;
-            while(a<=9){
-                System.out.println(input + " x " + a + " = " + (input*a));
-                a++;
+//        구구단 반복
+//        while(true){
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("구구단 단수를 입력해주세요");
+//            int input = sc.nextInt();
+//            int a = 1;
+//            while(a<=9){
+//                System.out.println(input + " x " + a + " = " + (input*a));
+//                a++;
+//            }
+//        }
+//
+////        도어락 while문 예제 : 비밀번호를 맞추면 반복이 종료되고 그렇지 않으면 계속적으로 다시 질문
+////        최대 5번 까지만 비밀번호 입력 가능하도록 => 입력횟수를 초과했습니다. 라는 문구 출력
+//
+//        String answer = "1234";
+//        int a=0;
+//        while(true){
+//            System.out.println("비밀번호를 입력해주세요.");
+//            Scanner sc = new Scanner(System.in);
+//            String input = sc.nextLine();
+//            if(answer.equals(input)){
+//                System.out.println("문이 열렸습니다.");
+//                break;
+//            }else{
+//                System.out.println("비밀번호가 틀렸습니다.");
+//                }
+//            a++;
+//            if(a==5){
+//                System.out.println("입력횟수를 초과했습니다.");
+//                break;
+//            }
+//        }
+
+////      2~10까지 출력하는 do while문 예제
+//        int a = 2;
+//        do {
+//            System.out.println(a);
+//            a++;
+//        }while (a<=10);
+
+////        for문을 통해 2~10까지 출력
+//        for(int i =2 ; i<=10; i++){
+//            System.out.println(i);
+//        }
+
+////        continue를 사용해서 홀수만 되도록 (1~10까지 수 중에)
+//        for(int i = 1; i<=10; i++){
+//            if(i%2 ==0){
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+////        배열과 enhance for문
+//        int[] myArr = {1,5,7,9,10};
+////        일반적인for문
+//        for(int i=0; i<5; i++){
+//            System.out.println(myArr[i]);
+//        }
+//////        enhance for문
+////        for(int a : myArr){
+////            System.out.println(a);
+////        }
+//
+////        일반 for문을 통해 myArr의 값에 10씩 더한 뒤에 출력
+//        for(int i =0; i<myArr.length; i++){
+//            myArr[i] += 10;
+//        }
+//        System.out.println(Arrays.toString(myArr));
+//
+////        enhance for문을 통해 myArr의 값에 10씩 더한 뒤에 출력
+//        for(int a : myArr){
+//            a += 10;
+//        }
+//        System.out.println(Arrays.toString(myArr));
+//
+////        java 변수의 유효범위 : { }
+//        int num = 10;
+//        if(num > 1){
+//            int abc = 20;
+//            num = 20;
+//        }
+//        System.out.println(num); // 20 if문 밖에서 선언된 변수 값을 if안에서 접근하여 변경하는 것은 가능.
+////        System.out.println(abc); if문 안에서 정의된 변수는 밖에서 인지 불가
+
+////        다중반복문을 이용한 구구단
+//
+//        for(int i = 2; i<=9 ; i++){
+//            System.out.println( i + "단입니다.");
+//            for(int j = 1; j<=9; j++){
+//                System.out.println(i + " x " + j + " = " + (i*j));
+//            }
+//        }
+
+//        2중 for을 통해 배열 접근
+        int[][] arr = {{1,2,3,4},{5,6,7,8}};
+        for(int i =0; i< arr.length; i++){
+            for(int j =0; j < arr[i].length; j++){
+                System.out.println(arr[i][j]);
             }
         }
+
 
 
     }
